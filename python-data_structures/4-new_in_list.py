b73431431327dacca8146dd1ece087f676a84a1c
hzeynalli07 <hzeynalli07@gmail.com>
2026-02-15 00:26:40 +0000

    Replace in a copy

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    # Orijinal siyahının surətini yaradırıq
     copy_list = my_list[:]
-    
     if idx < 0 or idx >= len(my_list):
         return copy_list
-    
     copy_list[idx] = element
     return copy_list
