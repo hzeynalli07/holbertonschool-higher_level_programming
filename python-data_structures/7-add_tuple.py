ad86d1866ed68f99889a5cdd4887a6eb76412f65
hzeynalli07 <hzeynalli07@gmail.com>
2026-02-15 00:33:51 +0000

    Adds 2 tuples

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    # Hər iki tuple-ı genişləndiririk: əgər az element varsa 0 əlavə edirik
-    # Slicing [:2] bizə yalnız ilk iki elementi verir
     a = tuple_a + (0, 0)
     b = tuple_b + (0, 0)
-    
     res_1 = a[0] + b[0]
     res_2 = a[1] + b[1]
-    
     return (res_1, res_2)
