c1d3d4647f7aa7adb13b9d1e815c30e56e5ade23
hzeynalli07 <hzeynalli07@gmail.com>
2026-02-22 14:15:05 +0000

    0. Integers addition with TDD

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+This module provides a function that adds two integers.
+The module contains one function: add_integer(a, b=98).
+"""
+
+
+def add_integer(a, b=98):
+    """
+    Adds two integers or floats.
+
+    Args:
+        a: The first number, must be an integer or float.
+        b: The second number, must be an integer or float. Default is 98.
+
+    Returns:
+        The integer sum of a and b.
+
+    Raises:
+        TypeError: If a or b are not integers or floats.
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
