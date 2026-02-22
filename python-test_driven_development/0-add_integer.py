cce4b84450f3a1daf8b8098d335f9b5e7dfc7ef4
hzeynalli07 <hzeynalli07@gmail.com>
2026-02-22 14:16:36 +0000

    Handle NaN and Infinity cases in add_integer

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -17,11 +17,17 @@ def add_integer(a, b=98):
         The integer sum of a and b.
 
     Raises:
-        TypeError: If a or b are not integers or floats.
+        TypeError: If a or b are not integers or floats, or if they are NaN/Inf.
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    # NaN və Infinity yoxlaması (çünki int() bunları çevirə bilmir)
+    if a != a or abs(a) == float('inf'):
+        raise TypeError("a must be an integer")
+    if b != b or abs(b) == float('inf'):
         raise TypeError("b must be an integer")
 
     return int(a) + int(b)
