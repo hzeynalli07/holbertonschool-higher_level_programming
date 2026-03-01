768a6bfa64025ab99e42d24cad369d90699d8793
hzeynalli07 <hzeynalli07@example.com>
2026-03-01 18:19:04 +0000

    Standardize output for Task 1

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -1,61 +1,48 @@
 #!/usr/bin/python3
 """
-Module for Shape, Circle, Rectangle and shape_info.
+Shapes module
 """
 from abc import ABC, abstractmethod
 import math
 
 
 class Shape(ABC):
-    """Abstract class Shape."""
-
+    """Abstract class Shape"""
     @abstractmethod
     def area(self):
-        """Abstract method area."""
         pass
 
     @abstractmethod
     def perimeter(self):
-        """Abstract method perimeter."""
         pass
 
 
 class Circle(Shape):
-    """Class Circle."""
-
+    """Circle class"""
     def __init__(self, radius):
-        """Initializes radius."""
         self.radius = radius
 
     def area(self):
-        """Calculates area."""
-        return math.pi * (self.radius ** 2)
+        return math.pi * self.radius ** 2
 
     def perimeter(self):
-        """Calculates perimeter."""
         return 2 * math.pi * self.radius
 
 
 class Rectangle(Shape):
-    """Class Rectangle."""
-
+    """Rectangle class"""
     def __init__(self, width, height):
-        """Initializes width and height."""
         self.width = width
         self.height = height
 
     def area(self):
-        """Calculates area."""
         return self.width * self.height
 
     def perimeter(self):
-        """Calculates perimeter."""
         return 2 * (self.width + self.height)
 
 
 def shape_info(shape):
-    """Duck typing shape_info."""
-    area = shape.area()
-    perimeter = shape.perimeter()
-    print("Area: {}".format(area))
-    print("Perimeter: {}".format(perimeter))
+    """Prints info about shape"""
+    print("Area: {}".format(shape.area()))
+    print("Perimeter: {}".format(shape.perimeter()))
