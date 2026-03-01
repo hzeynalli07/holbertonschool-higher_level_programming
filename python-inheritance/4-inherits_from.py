479c55dc575d15188d90b2ce00afe5553d62e056
hzeynalli07 <hzeynalli07@example.com>
2026-03-01 18:01:38 +0000

    Complete Task 4: Only sub class of

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 """
-Module for inherits_from function
+This module defines a function inherits_from.
 """
 
 
 def inherits_from(obj, a_class):
-    """Returns True if obj is an instance of a class that inherited from a_class"""
+    """
+    Returns True if the object is an instance of a class that 
+    inherited (directly or indirectly) from the specified class; 
+    otherwise False.
+    """
     return issubclass(type(obj), a_class) and type(obj) is not a_class
