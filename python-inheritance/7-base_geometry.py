3615bb722f772af7c68561c8f87d366429256c82
hzeynalli07 <hzeynalli07@example.com>
2026-03-01 17:49:46 +0000

    Strict documentation and type checking for task 7

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
-"""
-This module defines a class BaseGeometry.
-"""
+"""Defines a base geometry class BaseGeometry."""
 
 
 class BaseGeometry:
-    """A class with area and integer_validator methods."""
+    """Represent base geometry."""
 
     def area(self):
-        """Raises an Exception with the message area() is not implemented."""
+        """Not implemented."""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """
-        Validates value:
-        - name is always a string.
-        - if value is not an integer: raise a TypeError.
-        - if value is less or equal to 0: raise a ValueError.
+        """Validate a parameter as an integer.
+
+        Args:
+            name (str): The name of the parameter.
+            value (int): The parameter to validate.
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is <= 0.
         """
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
