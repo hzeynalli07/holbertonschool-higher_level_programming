ba494a3971227df461dbc0472a0eab5407e869cb
hzeynalli07 <hzeynalli07@example.com>
2026-03-01 17:45:09 +0000

    Fix: Strict type checking in integer_validator for task 7

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -8,11 +8,21 @@ class BaseGeometry:
     """Class with area and integer_validator methods"""
 
     def area(self):
-        """Raises an Exception as area is not implemented"""
+        """Raises an Exception because area is not implemented"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validates value as a positive integer"""
+        """
+        Validates value as a positive integer.
+        
+        Args:
+            name (str): The name of the value.
+            value (any): The value to validate.
+            
+        Raises:
+            TypeError: If value is not exactly an integer.
+            ValueError: If value is less than or equal to 0.
+        """
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
