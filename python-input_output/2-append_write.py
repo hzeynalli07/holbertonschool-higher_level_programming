be1b4545bb17e6f89e8e5fc07e60bb1520459e4b
hzeynalli07 <hzeynalli07@gmail.com>
2026-03-08 16:19:29 +0000

    Fix PEP8: line too long in 2-append_write.py

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""Faylın sonuna yazı əlavə edən funksiyanı təyin edir."""
+"""Faylın sonuna yazı əlavə edən funksiya."""
 
 
 def append_write(filename="", text=""):
-    """Mətni UTF8 formatında faylın sonuna əlavə edir və simvol sayını qaytarır."""
+    """Mətni UTF8 formatında fayla əlavə edir və sayını qaytarır."""
     with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
