40aab70be84c9457e899155b8325cf97504a0126
hzeynalli07 <hzeynalli07@gmail.com>
2026-03-08 16:51:23 +0000

    Add simple HTTP server API

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""http.server modulu ilə sadə API."""
+import http.server
+import json
+
+
+class SimpleAPIHandler(http.server.BaseHTTPRequestHandler):
+    """HTTP sorğularını idarə edən sinif."""
+
+    def do_GET(self):
+        """GET sorğularını emal edir."""
+        if self.path == '/':
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Hello, this is a simple API!")
+
+        elif self.path == '/data':
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            data = {"name": "John", "age": 30, "city": "New York"}
+            self.wfile.write(json.dumps(data).encode("utf-8"))
+
+        elif self.path == '/status':
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"OK")
+
+        elif self.path == '/info':
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            info = {"version": "1.0", "description": "A simple API built with http.server"}
+            self.wfile.write(json.dumps(info).encode("utf-8"))
+
+        else:
+            self.send_response(404)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Endpoint not found")
+
+
+def run(server_class=http.server.HTTPServer, handler_class=SimpleAPIHandler, port=8000):
+    """Serveri başladır."""
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f"Server {port} portunda işləyir...")
+    httpd.serve_forever()
+
+
+if __name__ == '__main__':
+    run()
