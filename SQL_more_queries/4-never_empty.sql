eda572d8ba4e6ee75f71b70bffc202f627b5ad75
hzeynalli07 <hzeynalli07@gmail.com>
2026-03-15 19:02:27 +0000

    Update task

---
+++ SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,2 @@
+-- Creates the table id_not_null with default value 1
+CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1, name VARCHAR(256));
---
+++ SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,2 @@
+-- Creates the table unique_id with unique id and default value 1
+CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
