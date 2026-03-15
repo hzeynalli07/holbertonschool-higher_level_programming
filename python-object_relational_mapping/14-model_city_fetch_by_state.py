41902566019761256546ce517bc22af7e71a7f2d
hzeynalli07 <hzeynalli07@gmail.com>
2026-03-15 19:22:04 +0000

    Update tasks

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
-"""Prints all City objects from the database"""
+"""
+Prints all City objects from the database hbtn_0e_14_usa
+"""
 import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 from model_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    results = session.query(City, State).filter(City.state_id == State.id)                     .order_by(City.id).all()
-
-    for city, state in results:
-        print("{}: ({}) {}".format(state.name, city.id, city.name))
-
+    for instance in (session.query(State, City)
+                     .filter(State.id == City.state_id)
+                     .order_by(City.id)
+                     .all()):
+        print("{}: ({}) {}".format(instance.State.name, instance.City.id,
+                                   instance.City.name))
     session.close()
